1658ca93b81adb70b66f84b1492e5d163474c161
Saeed Dehshiri <saeed@dehshiri.com>
2019-07-07 14:19:39 +0430

    update(home component): add loading in the home component when sent request

--- join-app-test/src/components/Home/Home.tsx
+++ join-app-test/src/components/Home/Home.tsx
@@ -2,18 +2,26 @@ import React from 'react';
 import './style.scss';
 import DatePicker from 'react-datepicker';
 import dateLogo from '../../assets/date.png'; // Tell Webpack this JS file uses this image
+import errorIcon from '../../assets/error.png';
+import loadingIcon from '../../assets/loading.svg';
  
 import 'react-datepicker/dist/react-datepicker.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomeItem from './components/HomeItem/HomeItem';
 import axios from 'axios';
+import { Button, InputGroup, FormControl, FormControlProps, Row, Col, Container } from 'react-bootstrap';
+import { ReplaceProps, BsPrefixProps } from 'react-bootstrap/helpers';
+
+enum RequestState {
+    None, Loading, Error, Success
+}
 
 type HomeState = {
     description: string;
     from: any;
     to: any;
     items: [];
-    loading: Boolean
+    requestState: RequestState 
 };
 
 class Home extends React.Component<{}, HomeState> {
@@ -22,27 +30,37 @@ class Home extends React.Component<{}, HomeState> {
         from: "",
         to: "",
         items: [],
-        loading: true
+        requestState: RequestState.None
     };
 
     componentDidMount() {
-        axios.get("https://bikewise.org/api/v2/incidents?page=1&proximity_square=100")
-        .then(res => {
-            console.log(res.data.incidents);
+        
+    }
+
+    onFormSubmit = async (e: React.SyntheticEvent) => {
+        e.preventDefault();
+        this.setState({
+            requestState: RequestState.Loading
+        });
+        this.state.requestState = RequestState.Loading;
+        try {
+            const res = await axios.get("https://bikewise.org/api/v2/incidents?page=1&proximity_square=100");
+            console.log(res.data);
             this.setState({
-                loading: false,
-                items: res.data.incidents
-            })
-        })
-        .catch(err => {
+                items: res.data.incidents,
+                requestState: RequestState.Success
+            });
+        } catch(err) {
+            console.log(err);
             this.setState({
-                loading: false
-            })
-        })
+                items: [],
+                requestState: RequestState.Error
+            });
+        }
     }
     
-    onChangeDescription = (e: React.FormEvent<HTMLInputElement>): void => {
-        this.setState({ description: e.currentTarget.value });
+    onChangeDescription = (e: React.FormEvent<ReplaceProps<"input", BsPrefixProps<"input"> & FormControlProps>>): void => {
+        this.setState({ description: e.currentTarget.value as string });
     };
 
     onChangeFrom = (e: React.FormEvent<HTMLInputElement>): void => {
@@ -73,68 +91,101 @@ class Home extends React.Component<{}, HomeState> {
         })
     };
 
+    renderBottom() {
+        switch(+this.state.requestState) {
+            case RequestState.None:
+            return (
+                <div>None</div>
+            );
+
+            case RequestState.Loading:
+            return (
+                <div>
+                    <img src={loadingIcon} />
+                </div>
+            );
+            
+            case RequestState.Error:
+            return (
+                <div><img alt="Error" src={errorIcon} /><p>Opps! Something went wrong :(</p></div>
+            );
+
+            case RequestState.Success:
+            return (
+                <div></div>
+            );
+        }
+    }
+
     render() {
         return (
             <div className="home">
-                <div className="home-search">
-                    <form
-                        //ref={formRef}
-                        onSubmit={(e: React.SyntheticEvent) => {
-                            e.preventDefault();
-                            
-                        }}
-                        >
-                        <input className="home-search__description-input" 
-                               type="text" 
-                               value={this.state.description} 
-                               onChange={this.onChangeDescription} 
-                               placeholder="description"
-                        />
-                        <DatePicker
-                            className="home-search__from-input"
-                            selected={ this.state.from }
-                            onChange={ this.handleDateFromChange }
-                            dateFormat="yyyy-MM-dd"
-                            placeholderText="From"
-                        />
-                        <img className="home-search__img" src={dateLogo} onClick={this.imageFromClicked} alt="" />
-                        <DatePicker
-                            className="home-search__to-input"
-                            selected={ this.state.to }
-                            onChange={ this.handleDateToChange }
-                            dateFormat="yyyy-MM-dd"
-                            placeholderText="To"
-                        />
-                        <img className="home-search__img" src={dateLogo} onClick={this.imageToClicked} alt="" />
-                        <input className="home-search__submit" type="submit" value="Find Cases" />
-                    </form>
+                <div className="home-top">
+                    <div className="home-search">
+                        <form
+                            //ref={formRef}
+                            onSubmit={this.onFormSubmit}
+                            >
+                            <Container>
+                            <Row>
+                                <Col>
+                                    <FormControl
+                                        placeholder="Description"
+                                        className="home-search__description-input" 
+                                        type="text" 
+                                        value={this.state.description} 
+                                        onChange={this.onChangeDescription} 
+                                    />
+                                </Col>
+                                <Col md="auto">
+                                    <InputGroup className="mb-3">
+                                        <DatePicker
+                                            className="home-search__from-input form-control"
+                                            selected={ this.state.from }
+                                            onChange={ this.handleDateFromChange }
+                                            dateFormat="yyyy-MM-dd"
+                                            placeholderText="From"
+                                        />
+                                        <InputGroup.Append>
+                                        <InputGroup.Text>
+                                            <img className="home-search__img" src={dateLogo} onClick={this.imageFromClicked} alt="" />
+                                        </InputGroup.Text>
+                                        </InputGroup.Append>
+                                    </InputGroup>
+                                </Col>
+                                <Col md="auto">
+                                    <InputGroup className="mb-3">
+                                        <DatePicker
+                                            className="home-search__to-input form-control"
+                                            selected={ this.state.to }
+                                            onChange={ this.handleDateToChange }
+                                            dateFormat="yyyy-MM-dd"
+                                            placeholderText="To"
+                                        />
+                                        <InputGroup.Append>
+                                        <InputGroup.Text>
+                                            <img className="home-search__img" src={dateLogo} onClick={this.imageFromClicked} alt="" />
+                                        </InputGroup.Text>
+                                        </InputGroup.Append>
+                                    </InputGroup>
+                                </Col>
+                                <Col md="auto">
+                                    <Button 
+                                        disabled={this.state.requestState == RequestState.Loading} 
+                                        variant="primary" 
+                                        className="home-search__submit" 
+                                        type="submit">
+                                            Find Cases
+                                    </Button>
+                                </Col>
+                            </Row>
+                            </Container>
+                        </form>
+                    </div>
                 </div>
-                {this.state.loading ? (
-                    <div>
-
-                    </div>
-                ):(
-                    <div className="home-items">
-                        {this.state.items.length === 0 ?
-                        (
-                            <div>No Result</div>
-                        )
-                        : 
-                        (<div className="home-items-top">
-                            <span className="home-items-top__total">total: {this.state.items.length}</span>
-                        {
-                            this.state.items.map(item => (
-                                <HomeItem itemData={item} />
-                            )) 
-                        }
-                        </div>)
-                        
-                        }
-                    </div>
-                )
-                }
-
-                
+                <div className="home-items">
+                    {this.renderBottom()}
+                </div>
             </div>
         );
     }
